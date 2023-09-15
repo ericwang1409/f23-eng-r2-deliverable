2876f4266783c640f757e9975d5a25b791f0c125
Eric Wang <eric3691215@gmail.com>
2023-09-14 23:35:54 -0400

    finished detailed view

--- app/species/species-card.tsx
+++ app/species/species-card.tsx
@@ -1,9 +1,22 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import type { Database } from "@/lib/schema";
 import Image from "next/image";
+import { useState } from "react";
 type Species = Database["public"]["Tables"]["species"]["Row"];
 
 export default function SpeciesCard(species: Species) {
+  const [open, setOpen] = useState<boolean>(false);
+
   return (
     <div className="min-w-72 m-4 w-72 flex-none rounded border-2 p-3 shadow">
       {species.image && (
@@ -15,7 +28,57 @@ export default function SpeciesCard(species: Species) {
       <h4 className="text-lg font-light italic">{species.scientific_name}</h4>
       <p>{species.description ? species.description.slice(0, 150).trim() + "..." : ""}</p>
       {/* Replace with detailed view */}
-      <Button className="mt-3 w-full">Learn More</Button>
+
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button className="mt-3 w-full" onClick={() => setOpen(true)}>
+            Learn More
+          </Button>
+        </DialogTrigger>
+        <DialogContent className="max-h-screen overflow-y-auto sm:max-w-[600px]">
+          <DialogHeader>
+            <DialogTitle>{species.common_name}</DialogTitle>
+            <DialogDescription>
+              More information on the {species.common_name}!
+            </DialogDescription>
+          </DialogHeader>
+          <div>
+            <h1 className="font-bold">Scientific Name</h1>
+            <p className="font-light italic">{species.scientific_name}</p>
+          </div>
+
+          <div>
+            <h1 className="font-bold">Common Name</h1>
+            <p className="font-light">{species.common_name}</p>
+          </div>
+
+          <div>
+            <h1 className="font-bold">Kingdom</h1>
+            <p className="font-light">{species.kingdom}</p>
+          </div>
+
+          <div>
+            <h1 className="font-bold">Total Population</h1>
+            <p className="font-light">{species.total_population ? species.total_population : "N/A"}</p>
+          </div>
+
+          <div>
+            <h1 className="font-bold">Description</h1>
+            <p className="font-light">{species.description}</p>
+          </div>
+
+          <div className="flex">
+            <Button 
+              type="button" 
+              className="ml-1 mr-1 flex-auto" 
+              variant="secondary" 
+              onClick={() => setOpen(false)}
+            >
+              Close
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
     </div>
   );
 }
